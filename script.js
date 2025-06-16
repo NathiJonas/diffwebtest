afd53c162cf318ed9c6d1e05d9fa2e01dcf5369b
Jonas Thamane <166150947+NathiJonas@users.noreply.github.com>
2025-06-16 16:44:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function login() {
-      const username = document.getElementById("username").value.trim();
-      const password = document.getElementById("password").value.trim();
-      if (username === "teacher" && password === "admin123") {
-        localStorage.setItem("loggedIn", true);
-        window.location.href = "dashboard.html";
-      } else {
-        document.getElementById("error").textContent = "Invalid credentials";
-      }
-    }
-    if (localStorage.getItem("loggedIn")) {
-      window.location.href = "dashboard.html";
-    }
-
 let learners = [];
 let sortAsc = true;
 
